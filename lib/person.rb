2da6d1bb7c794229bc093b3325546b9b265e256b
Meagan Utsey <meagan.utsey@gmail.com>
2020-01-12 19:26:14 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,9 @@
 class Person
 attr_accessor:
-jane_attributes = {name: "Jane", hair_color: "Brown"}
+jane_attributes = {name: "Jane", hair_color: "Brown", :eye_color: "Green"}
  
 Jane = Person.new(Jane_attributes)
 Jane.name       # => "Jane"
 Jane.hair_color  #=> "Brown"
+Jane.eye_color #=> "Green"
 end
