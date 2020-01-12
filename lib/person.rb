240ec50b9cba2c05e06e155b3ca2543ce64c702a
Meagan Utsey <meagan.utsey@gmail.com>
2020-01-12 19:32:09 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
-  #your code here
+attr_accessor:
+jane_attributes = {name: "Jane", hair_color: "Brown", :eye_color: "Green"}
+ 
+Jane = Person.new(Jane_attributes)
+Jane.name       # => "Jane"
+Jane.hair_color  #=> "Brown"
+Jane.eye_color #=> "Green"
+
+attr_accessor: 
+Carrie-attributes: = {name: "Carrie" , height: "5'5" , :handed "Left"} 
+
+Carrie= Person.new(Carrie_attributes)
+Carrie.name #=> "Carrie"
+Carrie.height #=> "5'5"
+Carrie.handed #=> "Left"
+
 end
