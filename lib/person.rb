5bb264e2da3919d18209b096690c0a0f17cbf92b
Meagan Utsey <meagan.utsey@gmail.com>
2020-01-12 19:25:12 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,8 @@
 class Person
-  
+attr_accessor:
+jane_attributes = {name: "Jane", hair_color: "Brown"}
+ 
+Jane = Person.new(Jane_attributes)
+Jane.name       # => "Jane"
+Jane.hair_color  #=> "Brown"
 end
