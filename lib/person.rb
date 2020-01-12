f1cd3db5d5ae1b83830b1e67f1f5b671d04193a0
Meagan Utsey <meagan.utsey@gmail.com>
2020-01-12 19:31:57 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -8,5 +8,11 @@ Jane.hair_color  #=> "Brown"
 Jane.eye_color #=> "Green"
 
 attr_accessor: 
-Carrie-attributes: = {name: "Carrie" , 
+Carrie-attributes: = {name: "Carrie" , height: "5'5" , :handed "Left"} 
+
+Carrie= Person.new(Carrie_attributes)
+Carrie.name #=> "Carrie"
+Carrie.height #=> "5'5"
+Carrie.handed #=> "Left"
+
 end
